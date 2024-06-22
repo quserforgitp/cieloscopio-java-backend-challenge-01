601c924801a77ab47f820f9f76b7a071bce37f5d
quserforgitp <adealumnonegligas@gmail.com>
2024-06-21 23:03:50 -0600

    feat(app):sparkles: add app menu add menu de la aplicacion | comprobar existen propiedades antes de seteo

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,10 @@ public class Main {
   public static void main(String[] args) throws IOException, InterruptedException {
 
     String apiKey = System.getenv("OPENWEATHERMAP_API_KEY");
+    if (apiKey == null || apiKey.isEmpty()) {
+      System.err.println("No se encontró la API key. Asegúrate de definir la variable de entorno OPENWEATHERMAP_API_KEY.");
+      return;
+    }
 
     // INPUT del nombre de la ciudad=========================================
     Scanner input = new Scanner(System.in);
@@ -19,6 +23,20 @@ public class Main {
     String cityName = "";
     String onlyNamesRgx = "[a-zA-Z\\s]+";
 
+    // WHILE DE LA APP
+
+      //1. mostrar menu (5 opciones => cd mex, morelia,caracas,buenos aires,brasilia,quito)
+      showMenu();
+
+      // pedir numero de opcion (while)
+
+      // pedir nombre de ciudad (validar con while)
+
+      // mostrar informacion
+
+      // goto 1.
+
+    // WHILE PARA INPUT DEL NOMBRE DE LA CIUDAD
     while (cityName.isBlank() || !cityName.matches(onlyNamesRgx)) {
       System.out.println("Ingrese el nombre de la ciudad");
       System.out.printf("> ");
@@ -35,7 +53,7 @@ public class Main {
     //FIN - INPUT del nombre de la ciudad=========================================
 
 
-    //============== GEO LOC REQ (get LAT and LONG by city name)
+    //============== GEO LOC REQ (get LAT and LONG by city name)=======================================================
     String URLgeoLocReq = "http://api.openweathermap.org/geo/1.0/direct?q="+
             cityNameFormatted+
             "&limit=1&appid="+apiKey;
@@ -49,29 +67,35 @@ public class Main {
 
     String geoLocData = geoLocResponse.body();
 
-    //System.out.println("\n======================GEO LOC DATA LAT & LONG==========================\n" + geoLocData + "\n==============================================");
-
     //==========EXTRAER DATOS DE LA RESPUESTA
     // parsear a jsonElement (manipulable, se puede convertir a tipos de datos json)
     JsonElement jsonElement = JsonParser.parseString(geoLocData);
-    //System.out.println("jsonElementGeoLoc======>"+ jsonElement);
 
     // parsear a array tipo json
     JsonArray jsonArray = jsonElement.getAsJsonArray();
 
     // obtener el objeto tipo json del array tipo json
+    if (jsonArray.isEmpty()) {
+      System.out.println("No se encontró información con el nombre de ciudad introducido: " + cityName);
+      System.err.println("abortando...");
+      return;
+    }
+
     JsonObject jsonObj = jsonArray.get(0).getAsJsonObject();
 
-    //System.out.println("jsonObject======================>"+jsonObj);
+    System.out.println("objeto json geo loc====================" + jsonObj);
 
     // Extraer datos del objeto tipo json como String's
     String lat = jsonObj.get("lat").getAsString();
     String lon = jsonObj.get("lon").getAsString();
-    String nameInSpanish = jsonObj.get("local_names").getAsJsonObject().get("es").getAsString();
-    //System.out.println("NAME IN SPANISH ========>======>"+nameInSpanish);
-    //System.out.println("===============\nlat = "+lat+"\n"+"lon = "+lon+"\n=================");
 
-    //============== WEATHER REQ (get WEATHER by LAT and LONG)
+    String nameInSpanish = jsonObj.get("name").getAsString();;
+    if (jsonObj.has("local_names"))
+      nameInSpanish = jsonObj.get("local_names").getAsJsonObject().get("es").getAsString();
+
+
+
+    //============== WEATHER REQ (get WEATHER by LAT and LONG)=========================================================
     HttpRequest weatherRequest = HttpRequest.newBuilder()
             .uri(URI.create("https://api.openweathermap.org/data/2.5/weather?lat="+lat+
                     "&lon="+lon+
@@ -83,14 +107,74 @@ public class Main {
 
     String weatherData = weatherResponse.body();
 
-    //System.out.println("\n=========================WEATHER DATA ==========================\n" + weatherData + "\n========================================");
+    JsonObject weatherJsonObj = JsonParser
+            .parseString(weatherData)
+            .getAsJsonObject();
+
+    //System.out.println("\n=========================WEATHER DATA ==========================\n" + weatherJsonObj + "\n========================================");
+
+    //CONDICION CLIMATICA
+    JsonObject condClimJsonObj = weatherJsonObj
+            .get("weather")
+            .getAsJsonArray()
+            .get(0)
+            .getAsJsonObject();
+
+    String condClimMain = condClimJsonObj
+            .get("main")
+            .getAsString();
+    String condClimDesc = condClimJsonObj
+            .get("description")
+            .getAsString();
+
+    String volumenLluvia = "...";
+
+    // si hay informacion de la lluvia, entonces setear a variable
+    if (weatherJsonObj.has("rain")) {
+      JsonObject lluviaJsonObj = weatherJsonObj.get("rain").getAsJsonObject();
+
+      volumenLluvia = lluviaJsonObj.has("1h") ?
+              lluviaJsonObj.get("1h").getAsString() :
+              lluviaJsonObj.get("3h").getAsString();
+      volumenLluvia += " Lts por metro cúbico";
+    }
+
+    // TEMPERATURAS
+    JsonObject tempJsonObj = weatherJsonObj
+            .get("main")
+            .getAsJsonObject();
+    String temp = tempJsonObj
+            .get("temp")
+            .getAsString();
+    String minTemp = tempJsonObj
+            .get("temp_min")
+                    .getAsString();
+    String maxTemp = tempJsonObj
+            .get("temp_max")
+                    .getAsString();
 
     System.out.printf("""
-=========== DATOS DEL CLIMA ===========
-"Nombre de la ciudad:%s
-"Latitud:%s
-"Longitud:%s
-=====================================""",nameInSpanish,lat,lon);
+            =============== RETRIEVED INFORMATION ===============
+            ciudad: %s
+            condicion: %s
+            descripción: %s
+            precipitación: %s
+            temperatura actual: %s
+            temperatura maxima: %s
+            temperatura minima: %s""",nameInSpanish,condClimMain,condClimDesc,volumenLluvia,temp,maxTemp,minTemp);
+  }
+  public static void showMenu () {
+    System.out.println("""
+            ========================= MENÚ =========================
+            | 1. Morelia (mx)                                     |
+            | 2. Caracas (vnz)                                    |
+            | 3. Buenos Aires (arg)                               |
+            | 4. Brasilia (br)                                    |
+            | 5. Quito (ec)                                       |
+            | 6. Introducir nombre manualmente                    |
+            | 7. Salir                                            |
+            ========================================================
+            """);
 
   }
 }
